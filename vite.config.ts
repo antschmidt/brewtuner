5417d7f56e37d77b213aee48deedce1b9930688e
Tony Schmidt <gnomadic@gmail.com>
2025-05-18 21:43:23 -0500

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [
+    sveltekit(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Coffee Dialer',
+        short_name: 'Dialer',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/<YOUR-SUBDOMAIN>\.nhost\.app\/.*$/,
+            handler: 'NetworkFirst',
+            options: { cacheName: 'api-cache' }
+          }
+        ]
+      }
+    })
+  ]
+};
+
+export default config;
